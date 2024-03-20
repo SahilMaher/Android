1ab56d0d58ac90adfb86efc1413a6a02ba3d3af6
unknown <admin@MCALAB17-149.mefgi.com>
2024-03-20 14:47:44 +0530

    Animation commit

--- Animations/app/src/main/java/com/example/animations/MainActivity.java
+++ Animations/app/src/main/java/com/example/animations/MainActivity.java
@@ -19,10 +19,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         alpha=findViewById(R.id.alpha);
+        tv=findViewById(R.id.textv);
         alpha.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Animation animation= AnimationUtils.loadAnimation(MainActivity.this,R.anim.alpha);
+                tv.startAnimation(animation);
+            }
+        });
+
+
+        routet=findViewById(R.id.routat);
+
+        routet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation animation= AnimationUtils.loadAnimation(MainActivity.this,R.anim.routet);
+                tv.startAnimation(animation);
+            }
+        });
+
+        scale=findViewById(R.id.scale);
+
+        scale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation animation= AnimationUtils.loadAnimation(MainActivity.this,R.anim.scale);
+                tv.startAnimation(animation);
+            }
+        });
+        translate=findViewById(R.id.translate);
+
+        translate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation animation= AnimationUtils.loadAnimation(MainActivity.this,R.anim.translate);
                 tv.startAnimation(animation);
             }
         });
